57336726f40d29756218b8612dd3cb37130f4b3e
Timur <tima.temirov.2005@mail.ru>
2024-05-19 13:55:43 -0400

    1

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -15,9 +15,20 @@ TEST(tests, test1) {
 }
 
 TEST(tests, test2) {
-
+    std::ostringstream oss;
+    std::streambuf* cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+    Dummy dummy(10);
+    std::cout.rdbuf(cout_streambuf);
+    EXPECT_EQ(oss.str(), "c'tor Dummy [10]\n");
 }
 
 TEST(tests, test3) {
-
+    std::ostringstream oss;
+    std::streambuf* cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+    Dummy dummy;
+    dummy.sayHello();
+    std::cout.rdbuf(cout_streambuf);
+    EXPECT_EQ(oss.str(), "c'tor Dummy [0]\nHello Dummy [0]\n");
 }
